ab1c31dc3b53f2d91828e841059e97872afdabda
Jose Hernandez <reyes_7374@outlook.es>
2025-09-13 18:09:22 -0600

    agregando archivos de modulo 4 y modulo 5

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,11 +1,18 @@
-function mostrarContenido(idElement, idBtn) {
+function mostrarContenido(idElement, idBtn, message = '') {
     let element = document.getElementById(idElement);
     let button = document.getElementById(idBtn);
     if (element.hasAttribute('hidden')) {
         element.removeAttribute('hidden');
-        button.textContent = 'ocultar contenido';
+        if (message === '')
+            button.textContent = 'ocultar contenido';
+        else
+            button.textContent = 'ocultar ' + message;
+
     } else {
         element.setAttribute('hidden', '');
-        button.textContent = 'mostrar contenido';
+        if (message === '')
+            button.textContent = 'mostrar contenido';
+        else
+            button.textContent = 'mostrar ' + message;
     }
 }
